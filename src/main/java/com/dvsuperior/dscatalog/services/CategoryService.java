59ad2905741e5b68e74fb6816d13bfd97cbb6cd5
Aquiles Valerio <aquilesvalerio@gmail.com>
2022-11-06 14:33:04 -0300

    Insert Category

--- src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/dvsuperior/dscatalog/services/CategoryService.java
@@ -29,7 +29,15 @@ public class CategoryService {
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
         Optional<Category> obj = repository.findById(id);
-        Category entity = obj.orElseThrow(()->new EntityNotFoundException("Entity not found"));
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO insertCategory(CategoryDTO dto) {
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
         return new CategoryDTO(entity);
     }
 }
